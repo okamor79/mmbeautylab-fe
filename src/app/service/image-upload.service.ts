ba0452063e54de3617f013e868a6bd18ce037a90
OKaminskyi <okamor@gmail.com>
2023-02-22 16:47:07 +0200

    Realese image-upload.service and notification.service.

--- src/app/service/image-upload.service.ts
+++ src/app/service/image-upload.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+const IMAGE_API = "http://localhost:8080/image/"
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageUploadService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  uploadImageToUser(file: File): Observable<any> {
+    const uploadData = new FormData();
+    uploadData.append('file', file);
+    return this.http.post(IMAGE_API + 'upload', uploadData);
+  }
+
+  uploadImageToCourse(file: File, courseId: number): Observable<any> {
+    const uploadData = new FormData();
+    uploadData.append('file', file);
+    return this.http.post(IMAGE_API + courseId + '/upload', uploadData);
+  }
+
+  getProfileImage(): Observable<any> {
+    return this.http.get(IMAGE_API + 'profileImage');
+  }
+
+  getImageToCourse(courseId: number): Observable<any> {
+    return this.http.get(IMAGE_API + courseId + '/image');
+  }
 }
--- src/app/service/notification.service.ts
+++ src/app/service/notification.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor() { }
+  constructor(private snakbar: MatSnackBar) { }
+
+  public showSnakBar(message: string): void {
+    // @ts-ignore
+    this.snakbar.open(message, null, {
+      duration: 2000
+    });
+  }
 }
